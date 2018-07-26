801c2df73d271c4b1ff092bbe043279b76b9118c
sean <584333869@qq.com>
2018-07-26 15:32:29 +0800

    自定义spring boot web socket

---
+++ springbootcustomwebsocket/src/main/java/com/voicecyber/SpringbootcustomwebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.voicecyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcustomwebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcustomwebsocketApplication.class, args);
+	}
+}
