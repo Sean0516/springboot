206acaad9366dcbfc5c63f8567e02e5d78824a6e
sean <584333869@qq.com>
2018-09-15 13:31:31 +0800

    spring  boot  rest controller  http headers add msg

---
+++ springbootrestserver/src/main/java/com/voicecyber/controller/RestUserController.java
@@ -0,0 +1,29 @@
+package com.voicecyber.controller;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+/**
+ * Created by Sean on 2018/9/15
+ *
+ * @author Sean
+ */
+@RestController
+public class RestUserController {
+
+    @RequestMapping(value = "getUserName")
+    public ResponseEntity userName(UriComponentsBuilder uriComponentsBuilder) {
+        URI getUserName = uriComponentsBuilder.path("getUserName")
+                .build().toUri();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(getUserName);
+        return new ResponseEntity("sean", httpHeaders, HttpStatus.OK);
+
+    }
+}
